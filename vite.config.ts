adeb0b4da5ca554d9f57eb28efdb5754cd7bd1d9
cpicondodtw <cpicondo@datawords.com>
2026-03-19 12:44:58 +0800

    Add github Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/sfcc-content-slot-generator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
